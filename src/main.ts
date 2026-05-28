9676b82061a2de0e97d5a177539ce176acde94db
nargesalnaji1-hue <nargesalnaji1@gmail.com>
2026-05-28 14:32:09 +0000

    Removed heroImg line

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import './style.css'
 import typescriptLogo from './assets/typescript.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
   <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
     <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
     <img src="${viteLogo}" class="vite" alt="Vite logo" />
   </div>
